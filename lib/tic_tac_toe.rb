f57df5fc29660a8f55102d129113a3f7e319a182
Sushi Chanrai <sushi@pathworks.co>
2018-06-15 22:32:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ end
 
 #turn
 def turn(board)
-  puts "Please enter 1-9:"
+  puts "Please enter a number between 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
